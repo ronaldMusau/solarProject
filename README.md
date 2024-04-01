3323d7e2a6e15c2bf0f3d666b837e6e0e87f7011
musauRonald <musauronald02@gmail.com>
2024-04-01 20:24:56 +0300

    updated my code to make it more refine

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ The code actually helps the solar panels to rotate in the direction of the sun t
 This is implemeted through the help of the LDR .
 
 It is still not yet a fine version ...yet to release a more defined version soon.
+
+#solar panel rotation 2 
+
+i made some chnages so as to soothen the rotation of the servo so as to reduce noise and for more accurate results.
--- suntrack.ino
+++ suntrack.ino
@@ -3,8 +3,8 @@
 Servo myservo1, myservo2;
 int LDR1 = A0, LDR2 = A1, LDR3 = A2, LDR4 = A3;
 int rRDL1 = 0, rRDL2 = 0, rRDL3 = 0, rRDL4 = 0;
-int max1=0, max2=0, max3=0;
-int ser1 = 80, ser2=0;
+int max3 = 0;
+int ser1 = 80, ser2 = 0;
 
 void setup() {
   myservo1.attach(9);
@@ -16,49 +16,56 @@ void setup() {
 }
 
 void loop() {
-
-  rRDL1 = analogRead(LDR1) / 100;
-  rRDL2 = analogRead(LDR2) / 100;
-  rRDL3 = analogRead(LDR3) / 100;
-  rRDL4 = analogRead(LDR4) / 100;
-
-  max1 = max(rRDL1, rRDL2);
-  max2 = max(rRDL3, rRDL4);
-  max3 = max(max1, max2);
   
-  //Serial.println(String(max3));
-  //Serial.println(String(rRDL1) +", "+String(rRDL2) +", "+String(rRDL3) +", "+String(rRDL4));
+  rRDL1 = smoothAnalogRead(LDR1) / 100;
+  rRDL2 = smoothAnalogRead(LDR2) / 100;
+  rRDL3 = smoothAnalogRead(LDR3) / 100;
+  rRDL4 = smoothAnalogRead(LDR4) / 100;
 
+  
+  max3 = max(max(rRDL1, rRDL2), max(rRDL3, rRDL4));
 
-  if(rRDL1<max3 && rRDL2<max3)
-  {
-    if(ser1<140)
-      ser1+=1;
-    myservo1.write(ser1);
-  }
-  if(rRDL3<max3 && rRDL4<max3)
-  {
-    if(ser1>0)
-      ser1-=1;
-    myservo1.write(ser1);
-  }
+  // Servo Control
+  adjustServoPosition();
 
-  if(rRDL2<max3 && rRDL3<max3)
-  {
-    Serial.println("servo2 +" + String(ser2));
-    if(ser2<180)
-      ser2+=1;
-    myservo2.write(ser2);
-  }
-  if(rRDL1<max3 && rRDL4<max3)
-  {
-    Serial.println("servo2 -" + String(ser2));
-    if(ser2>0)
-      ser2-=1;
-    myservo2.write(ser2);
-    
-  }
+  /
   delay(15);
+}
 
- 
+
+int smoothAnalogRead(int pin) {
+  const int numReadings = 10;
+  int total = 0;
+  for (int i = 0; i < numReadings; i++) {
+    total += analogRead(pin);
+    delay(1);
+  }
+  return total / numReadings;
+}
+
+
+void adjustServoPosition() {
+  
+  if (rRDL1 < max3 && rRDL2 < max3) {
+    if (ser1 < 140)
+      ser1 += 1;
+    myservo1.write(ser1);
+  } else if (rRDL3 < max3 && rRDL4 < max3) {
+    if (ser1 > 0)
+      ser1 -= 1;
+    myservo1.write(ser1);
+  }
+
+  
+  if (rRDL2 < max3 && rRDL3 < max3) {
+    Serial.println("servo2 +" + String(ser2));
+    if (ser2 < 180)
+      ser2 += 1;
+    myservo2.write(ser2);
+  } else if (rRDL1 < max3 && rRDL4 < max3) {
+    Serial.println("servo2 -" + String(ser2));
+    if (ser2 > 0)
+      ser2 -= 1;
+    myservo2.write(ser2);
+  }
 }
